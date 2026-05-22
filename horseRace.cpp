a6a9c4a7008475ff75a876eed07466ca6b26c261
carson <carson@nerdware.ca>
2026-05-21 20:43:47 -0700

    Check in of CLI horse race

---
+++ horseRace.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <Random.h>
+#include <string>
+#include <algorithm>
+
+constexpr int horse_count { 6 };
+constexpr std::string_view colours [horse_count] = {
+	"\033[38;5;9m",
+	"\033[38;5;10m",
+	"\033[38;5;11m",
+	"\033[38;5;12m",
+	"\033[38;5;13m",
+	"\033[38;5;14m"
+};
+
+constexpr std::string_view background = { "\033[38;5;0m" };
+
+constexpr int track_length_meters { 100 };
+constexpr std::string_view horsey { "h" };
+
+void moveCursorTo(int row, int col)
+{
+	printf("\033[%d;%dH", row, col);
+}
+
+void clearRowText(int row)
+{
+	moveCursorTo(row, 1);
+	printf("\033[K");
+}
+
+void replaceTextAtCursor(std::string_view r)
+{
+	printf("\b");
+	std::cout << r;
+	
+}
+int pickHorseToMove()
+{
+	return Random::get( 0 , horse_count - 1 );
+}
+
+void initTrack()
+{
+	moveCursorTo(1, 1);
+	std::cout << background;
+	std::cout << "\n";
+
+	for( int i { 0 }; i < horse_count; ++i )
+	{
+		for( int j { 0 }; j < track_length_meters; ++j )
+		{
+			std::cout << "-";
+		}
+		std::cout << "\n";
+	}
+	
+}
+
+int horseInTheLead(const int positions [horse_count] )
+{
+	//return *std::max_element( positions, positions + sizeof (positions[0]) );
+	int max_index = 0;
+	for ( int i { 1 }; i < horse_count; ++i )
+	{
+		if (positions[i] > positions[max_index])
+			max_index = i;
+	}
+	return max_index;
+
+}
+
+
+void updateTrack(const int positions [horse_count], const int moving_horse)
+{
+	const int in_the_lead = horseInTheLead(positions);
+	
+	if ( ( moving_horse != in_the_lead && positions[moving_horse] >= positions[in_the_lead] ) || positions[in_the_lead] == 1 || positions[in_the_lead] >= 100 )
+	{
+		clearRowText(1);
+		std::cout << colours[in_the_lead];
+		positions[in_the_lead] >= 100 ?
+			std::cout << "Horse " << in_the_lead + 1<< " has won the race!" :
+			std::cout << "Horse " << in_the_lead + 1<< " is in the lead!";
+		
+	}
+	
+	std::cout << colours[moving_horse];
+	moveCursorTo(moving_horse + 2, positions[moving_horse] + 1);
+	replaceTextAtCursor(horsey);
+	// so long as they arent on the first position (remember ansi starts on 1!) remove old the horse
+	if( positions[moving_horse] > 1 )
+	{
+		moveCursorTo(moving_horse + 2, positions[moving_horse] );
+		replaceTextAtCursor("-");
+	}
+	
+}
+
+int main()
+{
+	std::cout << std::unitbuf;
+	int positions [horse_count] = { 0, 0, 0, 0, 0, 0 };
+
+	bool race_over = false;
+
+	// Clear the screen
+	printf("\033[2J");
+	initTrack();
+	while(! race_over)
+	{
+		int moving_horse = pickHorseToMove();
+		// ensure this race aint over yet!
+		if ( positions[moving_horse] >= 100)
+		{
+			race_over = true;
+		}
+		else 
+		{
+			++positions[moving_horse];
+			updateTrack(positions, moving_horse);
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(25));
+	}
+	moveCursorTo(horse_count + 2, 1);
+	return 0;
+}
+
